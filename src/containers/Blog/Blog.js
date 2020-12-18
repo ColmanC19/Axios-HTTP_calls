1266d44ecf35741bee66ff1e09ea14e2699143f5
Colman Currie <curriecolman@gmail.com>
2020-12-18 11:15:40 -0800

    add state, use setState

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,16 +4,23 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import Axios from 'axios';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
     componentDidMount () {
-        Axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response)
+                this.setState({posts: response.data});
+                // console.log(response)
             });
     }
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post />
+        });
         return (
             <div>
                 <section className="Posts">
